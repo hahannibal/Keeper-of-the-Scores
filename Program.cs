6f4be14c327f01917555e0ec4c2c8d6c9aad7144
Kiss Tamás <Tamas.Kiss.87@hotmail.com>
2021-09-16 20:52:33 +0200

    added match to the program

--- Program.cs
+++ Program.cs
@@ -7,31 +7,43 @@ namespace Keeper_of_the_Scores
     {
         static void Main(string[] args)
         {
+            Match match = new Match();
+
             Team team1 = new Team();
             team1.Nationality = "Hungarian";
             team1.TeamName = "Hungary";
+            
             Player hungarianPlayer1 = new Player();
-            Player hungarianPlayer2 = new Player();
             hungarianPlayer1.Name = "Tamas";
             hungarianPlayer1.Number = 21;
+
+            Player hungarianPlayer2 = new Player();
             hungarianPlayer2.Name = "Dani";
             hungarianPlayer2.Number = 17;
+            
             team1.Players.Add(hungarianPlayer1);
             team1.Players.Add(hungarianPlayer2);
+
+            match.Team1 = team1;
 
             Team team2 = new Team();
             team2.Nationality = "Austrian";
             team2.TeamName = "Austria";
+            
             Player austrianPlayer1 = new Player();
-            Player austrianPlayer2 = new Player();
             austrianPlayer1.Name = "Florian";
             austrianPlayer1.Number = 07;
+
+            Player austrianPlayer2 = new Player();
             austrianPlayer2.Name = "Levi";
             austrianPlayer2.Number = 13;
+
             team2.Players.Add(austrianPlayer1);
             team2.Players.Add(austrianPlayer2);
 
-            Console.WriteLine($"{team1.TeamName} vs {team2.TeamName}");
+            match.Team2 = team2;
+
+            Console.WriteLine($"{match.Team1.TeamName} vs {match.Team2.TeamName}");
 
             Score score = new Score();
 
@@ -41,10 +53,16 @@ namespace Keeper_of_the_Scores
 
             List<Score> team1Score = new List<Score>();
             List<Score> team2Score = new List<Score>();
+
             team1Score.Add(score);
 
             Console.WriteLine($"Wow! {score.Scorer.Name} just made a point with the help of {score.Assister.Name} for {team1.TeamName}! The current score is: {team1Score.Count} : {team2Score.Count}");
             Console.WriteLine($"This happened at {score.DateTime}");
+
+            match.Team1Scores = team1Score;
+            match.Team2Scores = team2Score;
+
+
 
         }
     }
