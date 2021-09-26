5ba462cf147b1ccf1882f9d1932b86e867075af2
Kiss Tamás <Tamas.Kiss.87@hotmail.com>
2021-09-26 20:52:11 +0200

    added a counter to check how many teams are in the sheet

--- Data/GoogleSheets.cs
+++ Data/GoogleSheets.cs
@@ -26,42 +26,36 @@ namespace Keeper_of_the_Scores.Data
             List<Team> teams = new List<Team>();
 
             var teamNameRange = $"teams!B1:T1";
-            var teamNameRequest = service.Spreadsheets.Values.Get("1CUdylUqw3e3xnD0EYyIbzjwvDpqsIOIlK-edbQjgtOk", teamNameRange).Execute().Values;
+            var TeamName = service.Spreadsheets.Values.Get("1CUdylUqw3e3xnD0EYyIbzjwvDpqsIOIlK-edbQjgtOk", teamNameRange).Execute().Values;
+            
             var range = $"teams!B2:T30";
-            var request = service.Spreadsheets.Values.Get("1CUdylUqw3e3xnD0EYyIbzjwvDpqsIOIlK-edbQjgtOk", range).Execute().Values;
-            var counter = service.Spreadsheets.Values.Get("1CUdylUqw3e3xnD0EYyIbzjwvDpqsIOIlK-edbQjgtOk", $"teams!A30").Execute().Values;
-            int counterNumber = 0; 
-            foreach (var row in counter)
-            {
-                int i =Int32.Parse(row[0] as string);
-                counterNumber = i;
-            }
+            var PlayerNames = service.Spreadsheets.Values.Get("1CUdylUqw3e3xnD0EYyIbzjwvDpqsIOIlK-edbQjgtOk", range).Execute().Values;
+            int counter = TeamName[0].Count - 1; //counting the columns; -1, as the first column is the header
             
-            
-
-
-            for (int i = 0; i < counterNumber; i++) //it doesn't read the last team, have to fix it! 
+            for (int i = 0; i < counter; i++) 
             {
                 Team newTeam = new Team();
-                newTeam.Players = new List<Player>();
 
-                foreach (var row in teamNameRequest)
-                {
-                    newTeam.TeamName = row[i] as string;
-                }
-                foreach (var playerRow in request)
+                newTeam.Players = new List<Player>();
+                newTeam.TeamName = TeamName[0][i] as string;
+
+                Console.WriteLine(newTeam.TeamName);
+                foreach (var playerRow in PlayerNames)
                 {
                     Player player = new Player();
-                    player.Name = playerRow[i] as string;
-                    newTeam.Players.Add(player);
+                    if (playerRow.Count > i && playerRow[i] as string != "")
+                    {
+                        player.Name = playerRow[i] as string;
+                        newTeam.Players.Add(player);
+                    }
                 }
                 teams.Add(newTeam);
-                
 
-                
+
+
             }
             return teams;
-            }
+        }
 
     }
 }
