962aeedd3b607e78ee79b12b47b31e7e9dda1482
Kiss Tamás <Tamas.Kiss.87@hotmail.com>
2021-10-19 07:40:45 +0200

    grid is now displaying the right scorer and assist counting has to be fixed

--- Keeper of the Scores/Data/Score.cs
+++ Keeper of the Scores/Data/Score.cs
@@ -8,6 +8,14 @@ namespace Keeper_of_the_Scores
 {
     public class Score
     {
+        private Team _scoringTeam;
+
+        public Team ScoringTeam
+        {
+            get { return _scoringTeam; }
+            set { _scoringTeam = value; }
+        }
+
         public override string ToString()
         {
             return $"Scorer:{Scorer}; Assist:{Assist}";
