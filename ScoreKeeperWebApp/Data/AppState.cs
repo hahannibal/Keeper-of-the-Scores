f64493576f087b7eec225df5a689ca09ee4c533a
Kiss Tamás <Tamas.Kiss.87@hotmail.com>
2021-11-10 11:41:41 +0100

    created AppState

---
+++ ScoreKeeperWebApp/Data/AppState.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ScoreKeeperWebApp.Data
+{
+    public class AppState
+    {
+        public event Action OnChange;
+
+        public void ReloadGrid(Shared.TeamScoreDataGrid dataGrid)
+        {
+            dataGrid.Reload();
+        }
+
+        private void NotifyStateChanged() => OnChange?.Invoke();
+    }
+}
